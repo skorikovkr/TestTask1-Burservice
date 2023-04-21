00747939a04d5d6ea904c58994011f2ac5f8db9e
Kirill Skorikov <92673983+skorikovkr@users.noreply.github.com>
2023-04-21 20:57:38 +0500

    доделал сортировку, reverse

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net.Http.Headers;
+using TestTask1.Infrastructure;
 using TestTask1.Models;
 
 namespace TestTask1.Controllers
@@ -20,7 +21,23 @@ namespace TestTask1.Controllers
         [HttpPost]
         public IActionResult Sort(SortInfoModel model)
         {
-            ViewData["Sorted"] = model.Text.ToUpper();
+            var elements = new MyList<string>(model.Text.Split(",").Select(el => el.Trim()));
+            ViewData["Initial"] = String.Join(", ", elements);
+            switch (model.SortType)
+            {
+                case "reverse":
+                    elements.Reverse();
+                    break;
+                case "asc":
+                    elements.Sort();
+                    break;
+                case "desc":
+                    elements.Sort(false);
+                    break;
+                default:
+                    break;
+            }
+            ViewData["Sorted"] = String.Join(", ", elements);
             return this.View("Index");
         }
 
---
+++ Infrastructure/MyList.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TestTask1.Infrastructure
+{
+    public class MyList<T> : IEnumerable<T>
+        where T : IComparable<T> 
+    {
+        private List<T> _list = new List<T>();
+
+        public MyList(IEnumerable<T> elements)
+        {
+            foreach (var el in elements)
+                _list.Add(el);
+        }
+
+        public void Sort(bool asc = true) {
+            QuickSort(_list, 0, _list.Count - 1);
+            if (!asc) Reverse();
+        }
+
+        public void Reverse() { 
+            _list.Reverse();
+        }
+
+        private void QuickSort(List<T> list, int left, int right)
+        {
+            if (left >= right) return;
+            int newPivot = QuickSortHelper(list, left, right);
+            QuickSort(list, left, newPivot - 1);
+            QuickSort(list, newPivot + 1, right);
+        }
+
+        private int QuickSortHelper(List<T> list, int left, int right) {
+            var pivot = list[right];
+            var i = left;
+            for (int j = left; j < right; j++) {
+                if (list[j].CompareTo(pivot) <= 0) {
+                    var t = list[i];
+                    list[i] = list[j];
+                    list[j] = t;
+                    i++;
+                }
+            }
+            var temp = list[i];
+            list[i] = list[right];
+            list[right] = temp;
+            return i;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
